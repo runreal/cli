44f31e9f5c17567c46627ab2a52410962a6565e6
Stephen Lacy <me@slacy.me>
2024-04-04 10:43:02 -0400

    chore(cleanup): improve dep paths

--- src/deps.ts
+++ src/deps.ts
@@ -2,6 +2,7 @@ export * as path from 'std/path/mod.ts'
 export { mergeReadableStreams } from 'std/streams/merge_readable_streams.ts'
 export * as fmt from 'std/fmt/colors.ts'
 export * as dotenv from 'std/dotenv/mod.ts'
+export { parse } from 'std/jsonc/parse.ts'
 
 export { xml2js } from 'https://deno.land/x/xml2js@1.0.0/mod.ts'
 export { Command, EnumType, ValidationError } from 'https://deno.land/x/cliffy@v1.0.0-rc.3/command/mod.ts'
@@ -13,6 +14,5 @@ const ulid = monotonicFactory()
 export { ulid }
 
 export { z } from 'https://deno.land/x/zod/mod.ts'
-export { parse } from 'https://deno.land/std@0.221.0/jsonc/parse.ts'
 export { globber } from 'https://deno.land/x/globber@0.1.0/mod.ts'
 export { zodToJsonSchema } from 'npm:zod-to-json-schema'
