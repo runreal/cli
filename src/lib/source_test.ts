9ef81b0eeb97fc4d18b0e2f500c5657b2fd99f82
warman <warman@runreal.dev>
2024-04-11 12:20:01 -0400

    fix: p4 stream command

--- src/lib/source_test.ts
+++ src/lib/source_test.ts
@@ -1,23 +1,37 @@
 import { assertEquals } from 'https://deno.land/std@0.221.0/assert/mod.ts'
-import { Source } from './source.ts'
+import { Source, Perforce } from './source.ts'
 
-Deno.test('source', () => {
+Deno.test('source git', () => {
 	const source = Source('cwd', 'git')
 	assertEquals(source.executable, 'git')
 
 	source.ref = () => 'branch/commit'
 	assertEquals(source.safeRef(), 'branch-commit')
+
 	source.ref = () => 'branch//commit'
 	assertEquals(source.safeRef(), 'branch-commit')
+})
 
+Deno.test('source perforce - safeRef test', () => {
 	const psource = Source('cwd', 'perforce')
 	assertEquals(psource.executable, 'p4')
 
 	psource.ref = () => 'main/1'
 	assertEquals(psource.safeRef(), 'main-1')
+
 	psource.ref = () => 'main//1'
 	assertEquals(psource.safeRef(), 'main-1')
 
 	psource.ref = () => '//main//1'
 	assertEquals(psource.safeRef(), 'main-1')
+
+	psource.ref = () => '//Main//1'
+	assertEquals(psource.safeRef(), 'main-1')
+
+	const psource2 = new Perforce('cwd')
+	assertEquals(psource2.executable, 'p4')
+
+	psource2.stream = () => '//Stream/Main'
+	psource2.changelist = () => '50'
+	assertEquals(psource2.safeRef(), 'stream-main-50')
 })
