b2102e4c070659734afe4d2a46543f7a9ca21c5e
warman <warman@runreal.dev>
2024-05-28 15:43:37 -0400

    chore: fix up tests

--- tests/template.test.ts
+++ tests/template.test.ts
@@ -5,7 +5,11 @@ import { RunrealConfig } from '../src/lib/types.ts'
 Deno.test('template tests', () => {
 	const tmpl =
 		'{"name": "${project.name}", "engine": "${engine.path}\\BuildGraph\\Build.xml", "project": "${project.path}"}'
-	const cfg = { project: { name: 'Deno' }, engine: { path: 'C:\\Program Files\\V8' } } as RunrealConfig
+	const cfg = {
+		project: { name: 'Deno' },
+		engine: { path: 'C:\\Program Files\\V8' },
+		metadata: { ts: '2024-02-29T12:34:56Z' },
+	} as RunrealConfig
 
 	const result = render([tmpl], cfg)
 	assertEquals(result, [
@@ -20,6 +24,7 @@ Deno.test('getSubstitutions should correctly extract values from config', () => 
 		build: { id: '1234' },
 		buildkite: { buildNumber: '5678' },
 		metadata: {
+			ts: '2024-02-29T12:34:56Z',
 			safeRef: 'safeRef',
 			git: {
 				branch: 'longbranch',
@@ -43,6 +48,8 @@ Deno.test('getSubstitutions should correctly extract values from config', () => 
 		'metadata.git.commit': 'shortcommit',
 		'metadata.perforce.changelist': 'cl',
 		'metadata.perforce.stream': 'stream',
+		'metadata.ts': '2024-02-29T12:34:56Z',
+		'metadata.date': '2024-02-29',
 	}
 	const result = getSubstitutions(cfg)
 	assertEquals(result, expected)
@@ -53,16 +60,21 @@ Deno.test('render should replace placeholders with correct values', () => {
 		'${project.name} uses ${engine.path}',
 		'Build ID: ${build.id}',
 		'Non-existent: ${non.existent}',
+		'Timestamp: ${metadata.ts}',
+		'Date: ${metadata.date}',
 	]
 	const cfg: Partial<RunrealConfig> = {
 		project: { name: 'Project', path: '/projects/project', repoType: 'git', buildPath: '/output/path' },
 		engine: { path: '/engines/5.1', repoType: 'git' },
 		build: { id: '1234' },
+		metadata: { ts: '2024-02-29T12:34:56Z' },
 	}
 	const expected = [
 		'Project uses /engines/5.1',
 		'Build ID: 1234',
 		'Non-existent: ${non.existent}',
+		'Timestamp: 2024-02-29T12:34:56Z',
+		'Date: 2024-02-29',
 	]
 	const result = render(input, cfg as RunrealConfig)
 	assertEquals(result, expected)
@@ -73,6 +85,7 @@ Deno.test('renderConfig should deeply replace all placeholders in config object'
 		project: { name: 'Project', path: '/projects/project', repoType: 'git', buildPath: '/output/path' },
 		engine: { path: '/engines/5.0', repoType: 'git' },
 		build: { id: '1234' },
+		metadata: { ts: '2024-02-29T12:34:56Z' },
 		workflows: [
 			{
 				name: 'compile',
@@ -81,7 +94,7 @@ Deno.test('renderConfig should deeply replace all placeholders in config object'
 						command: 'build',
 						args: [
 							'${project.path}\\Build\\Build.xml',
-							'-set:BuildId=${build.id}',
+							'-set:BuildId=${build.id}-${metadata.date}',
 							'-set:ProjectName=${project.name}',
 						],
 					},
@@ -93,6 +106,7 @@ Deno.test('renderConfig should deeply replace all placeholders in config object'
 		project: { name: 'Project', path: '/projects/project', repoType: 'git', buildPath: '/output/path' },
 		engine: { path: '/engines/5.0', repoType: 'git' },
 		build: { id: '1234' },
+		metadata: { ts: '2024-02-29T12:34:56Z' },
 		workflows: [
 			{
 				name: 'compile',
@@ -101,7 +115,7 @@ Deno.test('renderConfig should deeply replace all placeholders in config object'
 						command: 'build',
 						args: [
 							'/projects/project\\Build\\Build.xml',
-							'-set:BuildId=1234',
+							'-set:BuildId=1234-2024-02-29',
 							'-set:ProjectName=Project',
 						],
 					},
@@ -118,6 +132,7 @@ Deno.test('replace paths in template', () => {
 		project: { name: 'Project', path: '/projects/project', repoType: 'git', buildPath: '/output/path' },
 		engine: { path: '/engines/5.0', repoType: 'git' },
 		build: { id: '1234' },
+		metadata: { ts: '2024-02-29T12:34:56Z' },
 		workflows: [
 			{
 				name: 'compile',
