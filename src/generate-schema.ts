3edb37cdbf60200e8e9ed313abb11bd09c805207
Thibault Le Ouay <thibaultleouay@gmail.Com>
2024-09-24 10:24:39 +0200

    fix: run generate

--- src/generate-schema.ts
+++ src/generate-schema.ts
@@ -3,7 +3,7 @@ import { zodToJsonSchema } from './deps.ts'
 import { ConfigSchema } from './lib/schema.ts'
 
 const schema = zodToJsonSchema(ConfigSchema, {
-	target: 'jsonSchema2019-09',
+	target: 'jsonSchema7',
 })
 
 await Deno.writeTextFile('schema.json', JSON.stringify(schema, null, 2))
