030369fe5c6d7fa7952dd894b4f90efe47d68d6e
Thibault Le Ouay <thibaultleouay@gmail.Com>
2024-10-31 11:45:54 +0100

    fix: buildkite pipeline

--- src/deps.ts
+++ src/deps.ts
@@ -8,7 +8,7 @@ export { parse } from '@std/jsonc'
 // Maybe use npm specifier
 
 export { xml2js } from 'https://deno.land/x/xml2js@1.0.0/mod.ts'
-export { Command, EnumType, ValidationError } from '@cliffy/command'
+export { Command, EnumType, ValidationError } from 'jsr:@cliffy/command@1.0.0-rc.7'
 export { deepmerge } from 'jsr:@rebeccastevens/deepmerge'
 export { readNdjson } from 'https://deno.land/x/ndjson@1.1.0/mod.ts'
 
