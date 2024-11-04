c1d9b579b706c56d1ebdd246610552c343a81fa0
warman <warman@runreal.dev>
2024-11-03 19:09:02 -0500

    Fix merge main

--- src/commands/workflow/exec.ts
+++ src/commands/workflow/exec.ts
@@ -56,7 +56,7 @@ export const exec = new Command<GlobalOptions>()
 	.arguments('<workflow>')
 	.action(async (options, workflow) => {
 		const { dryRun, mode } = options
-		const cfg = config.get(options as CliOptions) as any
+		const cfg = config.get(options as CliOptions)
 
 		if (!cfg.workflows) {
 			throw new ValidationError('No workflows defined in config')
