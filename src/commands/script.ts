97303856d1d516349b5c858d601f033a284a55d6
Thibault Le Ouay <thibaultleouay@gmail.Com>
2024-11-11 15:37:58 +0100

    chore: remove dead code

--- src/commands/script.ts
+++ src/commands/script.ts
@@ -2,26 +2,15 @@ import { $, Command, path } from '../deps.ts'
 import type { Script, ScriptContext } from '../lib/types.ts'
 import { logger } from '../lib/logger.ts'
 import { config } from '../lib/config.ts'
-// utils
-// import * as esbuild from "npm:esbuild@0.24.0";
+
 import * as esbuild from 'https://deno.land/x/esbuild@v0.24.0/mod.js'
 
-// import { importString } from 'https://deno.land/x/import/mod.ts';
-
-// import * as esbuild from "https://deno.land/x/esbuild@0.24.0/wasm.js";
-// Import the Wasm build on platforms where running subprocesses is not
-// permitted, such as Deno Deploy, or when running without `--allow-run`.
-// import * as esbuild from "https://deno.land/x/esbuild@0.20.2/wasm.js";
 
 import { denoPlugins } from 'jsr:@luca/esbuild-deno-loader@0.11.0'
 
-esbuild.stop()
 export const script = new Command()
 	.description('script')
 	.arguments('<input:string>')
-	// .parse(Deno.args)
-	// .useRawArgs() // <-- enable raw args
-	// .arguments("[script] [...args]")
 	.action(async (options, ...args: string[]) => {
 		if (!args[0]) {
 			logger.error('No script name provided')
