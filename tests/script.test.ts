f1fec97864c936181f28a4e9dd2d6cb42ef728c1
warman <warman@runreal.dev>
2025-05-08 14:47:26 -0400

    chore: test path fix

--- tests/script.test.ts
+++ tests/script.test.ts
@@ -1,10 +1,11 @@
 import { snapshotTest } from '@cliffy/testing'
 import { script } from '../src/commands/script.ts'
+import * as path from '@std/path'
 
 await snapshotTest({
-	name: 'should execute the command ',
+	name: 'should execute the command',
 	meta: import.meta,
-	args: ['./tests/fixtures/hello-world.ts'],
+	args: ['tests/fixtures/hello-world.ts'],
 	denoArgs: ['-A'],
 	async fn() {
 		await script.parse()
